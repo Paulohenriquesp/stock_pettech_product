be45485807306c39cfc4165ac768afce2339e6b1
Paulo <paulosouzap7@gmail.com>
2025-02-17 21:50:03 -0300

    feat: creation product interface and cfg eslint

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,12 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { StockModule } from './stock/stock.module';
+import mongoose from 'mongoose';
+import { MongooseModule } from '@nestjs/mongoose';
 
 @Module({
-  imports: [],
+  imports: [MongooseModule.forRoot('mongodb://localhost/pettech'), StockModule],
   controllers: [AppController],
   providers: [AppService],
 })
